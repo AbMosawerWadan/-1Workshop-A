3b647be4cd4c58d0e38828e74a239c76cf23ed39
Abdul Mosawer Wadan <vadanmosawer@gamil.com>
2024-10-02 22:40:42 +0330

    Progress of programm

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,11 @@ public class Calculator{
 
         System.out.print("Please Enter Your Operator(* , / , + , -)");
         operator = sc.next().charAt(0);
+
+        int  sum = Fn + Sn ;
+        int  product = Fn * Sn;
+        Double Division = (double)(Fn / Sn);
+        int substraction = Fn - Sn;
         
 
     }

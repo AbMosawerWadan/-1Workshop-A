0f132a04b8baa89c6414c8686ce99588163c88e3
Abdul Mosawer Wadan <vadanmosawer@gmail.com>
2026-06-06 16:23:00 +0430

    Refactor calculator to use MathOperations class

--- Calculator.java
+++ Calculator.java
@@ -1,54 +1,63 @@
 import java.util.Scanner;
-public class Calculator{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         char operator;
+class MathOperations {
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-        System.out.print("Enter First Number:");
-        int Fn = sc.nextInt();
-        System.out.print("Enter the Secound Number:");
-        int Sn = sc.nextInt();
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 
-        System.out.print("Please Enter Your Operator(* , / , + , -)--->");
-        operator = sc.next().charAt(0);
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
-        int  sum = Fn + Sn ;
-        int  product = Fn * Sn;
-        double Division = 0;
-        if(Sn != 0){
-            Division = (double) (Fn / Sn);
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("You cannot divide by zero!");
         }
-        int substraction = Fn - Sn;
-        
-
-        switch(operator){
-            case ('+'):
-            System.out.print("The sum is ="+sum);
-            break;
-            
-            case ('-'):
-            System.out.println("The substraction = "+substraction);
-            break;
-
-            case ('/'):
-            if(Sn  == 0){
-                System.out.println("You cant divide (0 / 0)!");
-            }
-            else
-            {
-            System.out.println("The Division is = "+Division);
-            }
-            break;
-
-            case ('*'):
-            System.out.println("The Product is = " +product);
-            break;
-            default:
-            System.out.println("Error!!!");
-        }    
-
-
-
+        return (double) a / b;
     }
 }
- 
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MathOperations mathOps = new MathOperations();
+
+        System.out.print("Enter First Number: ");
+        int fn = sc.nextInt();
+
+        System.out.print("Enter the Second Number: ");
+        int sn = sc.nextInt();
+
+        System.out.print("Please Enter Your Operator (* , / , + , -) ---> ");
+        char operator = sc.next().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.println("The sum is = " + mathOps.add(fn, sn));
+                break;
+                
+            case '-':
+                System.out.println("The subtraction = " + mathOps.subtract(fn, sn));
+                break;
+                
+            case '*':
+                System.out.println("The product is = " + mathOps.multiply(fn, sn));
+                break;
+                
+            case '/':
+                if (sn == 0) {
+                    System.out.println("You can't divide (0 / 0)!");
+                } else {
+                    System.out.println("The division is = " + mathOps.divide(fn, sn));
+                }
+                break;
+                
+            default:
+                System.out.println("Error!!! Invalid Operator.");
+        }
+
+        sc.close();
+    }
+}
